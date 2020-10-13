9aad14fecbaa037e0b0faf5eeba71a0688ec795e
Gonzalo Manzzi <gonzalo427@hotmail.com>
2020-10-13 16:22:53 -0300

    Implementación hasta la parte 2

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using CompAndDel.Pipes;
+using CompAndDel.Filters;
 
 namespace CompAndDel
 {
@@ -6,6 +8,26 @@ namespace CompAndDel
     {
         static void Main(string[] args)
         {
+
+            PictureProvider provider = new PictureProvider();
+            IPicture picture = provider.GetPicture("../../assets/yacht.jpg");
+
+            IPipe pipeNull = new PipeNull();
+            IFilter filterSavePicture = new FilterSavePicture();
+
+            IPipe secondPipeSerialSave = new PipeSerial(filterSavePicture, pipeNull);
+
+            IFilter filterNegative = new FilterNegative();
+            IPipe secondPipeSerial = new PipeSerial(filterNegative, secondPipeSerialSave);
+
+            
+            IPipe firstPipeSerialSave = new PipeSerial(filterSavePicture, secondPipeSerial);
+
+            IFilter filterGreyScale = new FilterGreyscale();
+            IPipe pipeSerial = new PipeSerial(filterGreyScale, firstPipeSerialSave);
+
+            IPicture newPicture = pipeSerial.Send(picture);
+            provider.SavePicture(newPicture, "picture.jpg");
         }
     }
 }
